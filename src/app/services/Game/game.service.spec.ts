b76ffc92153c779617087aa15ebdec50fa0a30d2
EricHoffmann57 <81462264+EricHoffmann57@users.noreply.github.com>
2022-01-09 19:25:52 +0100

    Update game.service.spec.ts

--- src/app/services/Game/game.service.spec.ts
+++ src/app/services/Game/game.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { GameService } from './game.service';
 
-describe('FilmService', () => {
+describe('GameService', () => {
   let service: GameService;
 
   beforeEach(() => {
